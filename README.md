d6cec4572907d8b73617d0d23ef74c8f4a75121c
Peter StJ <regardingscot@gmail.com>
2014-10-20 09:58:19 +0300

    Initial import

---
+++ README.md
@@ -0,0 +1,11 @@
+###Externs for the closure compiler
+
+
+Provides the externs used in my projects.
+
+_The following projects are included currently:_
+
+- pstj
+
+
+Additional externs will be included as the project goes.
---
+++ input.js
@@ -0,0 +1,40 @@
+/**
+ * Provides the InputSetting JSON typed object for constructing an input
+ * element imperatively from the server.
+ */
+
+
+
+/**
+ * The material setup object
+ * @constructor
+ */
+function MaterialInputConfig() {}
+
+
+/** @type {string|number} */
+MaterialInputConfig.prototype.id;
+
+
+/** @type {string} */
+MaterialInputConfig.prototype.name;
+
+
+/** @type {string} */
+MaterialInputConfig.prototype.type;
+
+
+/** @type {string} */
+MaterialInputConfig.prototype.label;
+
+
+/** @type {string} */
+MaterialInputConfig.prototype.value;
+
+
+/** @type {string} */
+MaterialInputConfig.prototype.pattern;
+
+
+/** @type {string} */
+MaterialInputConfig.prototype.errorText;
